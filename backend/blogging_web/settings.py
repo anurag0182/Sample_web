1b75ea2470f9cba0c3408d9e726f9498d26ba8ab
Anurag Singh <anuragsinghtomar092@gmail.com>
2026-02-23 14:14:53 +0530

    Add STATIC_ROOT configuration for static files

--- backend/blogging_web/settings.py
+++ backend/blogging_web/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 CORS_ALLOW_ALL_ORIGINS = True
